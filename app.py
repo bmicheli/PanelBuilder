3958c20aff27ce5dc188ac52589eb7b4e53f8847
bmicheli <32937380+bmicheli@users.noreply.github.com>
2025-07-04 11:09:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1533,9 +1533,9 @@ app.clientside_callback(
 # APP RUN
 # =============================================================================
 
-if __name__ == "__main__":
-	app.run(debug=True)
+#if __name__ == "__main__":
+#	app.run(debug=True)
 
-# if __name__ == '__main__':
-# 	port = int(os.environ.get("PORT", 8050))
-# 	app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+	port = int(os.environ.get("PORT", 8050))
+	app.run(host="0.0.0.0", port=port)
